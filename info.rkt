f879a5582004ca8615efcd474f8536781a15eb63
TheMetaSchemer <anurag@mendhekar.com>
2023-03-01 19:17:57 -0800

    Updated version number to reflect new validation changes.

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "malt")
 (define deps '(["base" #:version "8.2"] "rackunit-lib"))
 (define pkg-desc "A MAchine Learning Toolkit accompanying The Little Learner: A Straight Line to Deep Learning by Daniel P. Friedman and Anurag Mendhekar")
-(define version "0.1.0")
+(define version "0.1")
 (define compile-omit-paths (list #rx"test\\\\" #rx"test/"))
 (define test-omit-paths (list #rx"test\\\\" #rx"test/"))
 (define pkg-authors '("Anurag Mendhekar" "Daniel P. Friedman"))
