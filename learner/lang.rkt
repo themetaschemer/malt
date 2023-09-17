cb9b6e837e466b561320b4795190de308d04986c
Sam Phillips <samdphillips@gmail.com>
2023-09-17 13:04:00 -0700

    Setup `malt/learner` as a lang

---
+++ learner/lang.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(require malt/ports
+         malt/learner)
+
+(provide (all-from-out racket/base)
+         (all-from-out malt/learner))
---
+++ learner/lang/reader.rkt
@@ -0,0 +1,2 @@
+#lang s-exp syntax/module-reader
+malt/learner/lang
