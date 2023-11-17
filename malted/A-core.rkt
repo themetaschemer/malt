0f980c62546ca862eaed9357844f677d1eefb6eb
Darshal Shetty <darshalcs@gmail.com>
2023-11-17 12:30:51 -0500

    [add-lazy]Improve caching in gradient descent

--- malted/A-core.rkt
+++ malted/A-core.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
 (require "../base.rkt")
+;; TODO: This is not implementation independent. Figure out a fix
+(require (only-in "../lazy/tensors.rkt" ↓))
 
 (define dot-product
   (λ (w t)
@@ -14,4 +16,4 @@
 
 (include "test/test-A-core.rkt")
 
-(provide dot-product dot-product-2-1)
+(provide dot-product dot-product-2-1 ↓)
