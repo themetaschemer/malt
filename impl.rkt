58220bb172458c37b592024907a1a35a001486aa
Darshal Shetty <darshalcs@gmail.com>
2024-01-31 07:49:03 -0500

    [add-unif]Add uniform-tensors representation using f32vector

--- impl.rkt
+++ impl.rkt
@@ -13,10 +13,12 @@
         #,(case (tensor-implementation)
             ((learner) #'(require "learner.rkt"))
             ((flat-tensors) #'(require "flat-tensors.rkt"))
+            ((uniform-tensors) #'(require "uniform-tensors.rkt"))
             ((nested-tensors) #'(require "nested-tensors.rkt")))
         #,(case (tensor-implementation)
             ((learner) #'(provide (all-from-out "learner.rkt")))
             ((flat-tensors) #'(provide (all-from-out "flat-tensors.rkt")))
+            ((uniform-tensors) #'(provide (all-from-out "uniform-tensors.rkt")))
             ((nested-tensors) #'(provide (all-from-out "nested-tensors.rkt")))))))
 
 (load-tensors)
